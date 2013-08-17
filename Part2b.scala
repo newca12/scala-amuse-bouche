53e8418347afd4f4efb21db1f0bab35910384348
Olivier ROLAND <olivier.roland@edla.org>
2013-08-17 18:42:34 +0200

    Part2b done

---
+++ Part2b.scala
@@ -0,0 +1,18 @@
+val empty = Nil
+val oneWord = "apple" :: Nil
+val twoWords = "banana" :: "cantaloupe" :: Nil
+
+val mystery1 = "pear" :: empty
+val mystery2 = "peach" :: oneWord
+lazy val mystery3 :List[String] = "pineapple" :: mystery3
+val mystery4 = 42 :: "apple" :: Nil //This is OK with Scala :-)
+
+def dropOne[T](l: List[T]) = l match {
+  case Nil => Nil
+  case (first :: rest) => rest
+}   
+
+def justOne[T](l: List[T]) = l match {
+  case Nil => Nil
+  case (first :: rest) => first
+}
