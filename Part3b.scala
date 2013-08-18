969b18b6b50da4774df17e18c6ecbab8d277251b
Olivier ROLAND <olivier.roland@edla.org>
2013-08-18 17:58:13 +0200

    Part3b done

---
+++ Part3b.scala
@@ -0,0 +1,20 @@
+val tvShows =  Map((1966, "Star Trek") , (1969, "Monty Python's Flying Circus") , (1989, "The Simpsons"))
+
+def showForYear(y: Int) = tvShows.get(y)
+
+def showWithName(n: String) = tvShows collectFirst { case (a,b) if (b.contains(n)) => b }
+
+def favoriteShow(s: String) = s match {
+  case "Amy" => Some("Batman")
+  case "Bob" => Some("Iron Chef")
+  case _ => None
+}
+
+case class Person(val name :String, val year :Int)
+
+val amy = Person(name = "Amy", year = 1971)
+val cam = Person(name = "Cam", year = 1989)
+val deb = Person(name = "Deb", year = 1967)
+val monty = Person (name = "Monty", year = 1973)
+
+def picShow(p: Person) = favoriteShow(p.name).orElse(showWithName(p.name).orElse(showForYear(p.year)))
